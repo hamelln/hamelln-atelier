ce0b006d169f096c22a51ae7bb26503d7e034647
hamelln <vusdus91@gmail.com>
2023-03-16 22:14:08 +0900

    feat: 테마 버튼 클릭 시 효과음

---
+++ src/themeButton/sound.js
@@ -0,0 +1,39 @@
+const handleSoundSign = (() => {
+  const setup = () => {
+    const morningSound = document.getElementById("morning-sound");
+    const nightSound = document.getElementById("night-sound");
+    const soundMap = new Map();
+    soundMap.set(1, morningSound);
+    soundMap.set(-1, nightSound);
+    return soundMap;
+  };
+
+  const sound = () => soundMap.get(soundSign);
+
+  const play = (sound) => {
+    sound.play();
+  };
+
+  const stop = (sound) => {
+    sound.pause();
+    sound.currentTime = 0;
+  };
+
+  const setSoundSign = (newSoundSign) => {
+    if (soundSign !== newSoundSign) {
+      stop(sound());
+      soundSign = newSoundSign;
+      play(sound());
+    }
+  };
+
+  const handleSoundSign = () => {
+    setSoundSign(-soundSign);
+  };
+
+  let soundSign = 1;
+  const soundMap = setup();
+  return handleSoundSign;
+})();
+
+export default handleSoundSign;
