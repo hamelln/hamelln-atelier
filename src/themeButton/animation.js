c99f8cb59cab9a685bca2dc65440ca34934daae3
hamelln <vusdus91@gmail.com>
2023-03-16 22:20:14 +0900

    fix: 필요한 준비 요소들은 setup에서 만들도록 변경.

--- src/themeButton/animation.js
+++ src/themeButton/animation.js
@@ -1,7 +1,48 @@
 import Particle from "./Particle.js";
 
 export const { handleAnimation, stopAnimation } = (() => {
-  const init = () => {
+  const setup = () => {
+    const canvas = document.querySelector(".mode__canvas");
+    const ctx = canvas.getContext("2d", { willReadFrequently: true });
+    canvas.width = 140;
+    canvas.height = 80;
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+
+    const getRgbsFromImageData = (imageArray, WIDTH, HEIGHT) => {
+      const colorMap = [];
+      for (let y = 0; y < HEIGHT; y++) {
+        const row = [];
+        for (let x = 0; x < WIDTH; x++) {
+          const r = imageArray.data[y * 4 * imageArray.width + x * 4];
+          const g = imageArray.data[y * 4 * imageArray.width + x * 4 + 1];
+          const b = imageArray.data[y * 4 * imageArray.width + x * 4 + 2];
+          const color = `rgb(${r},${g},${b})`;
+          row.push(color);
+        }
+        colorMap.push(row);
+      }
+      return colorMap;
+    };
+
+    const night = document.getElementById("night");
+    const morning = document.getElementById("morning");
+
+    ctx.drawImage(night, 0, 0, WIDTH, HEIGHT);
+    const nightData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+    const nightGrid = getRgbsFromImageData(nightData, WIDTH, HEIGHT);
+
+    ctx.drawImage(morning, 0, 0, WIDTH, HEIGHT);
+    const morningData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+    const morningGrid = getRgbsFromImageData(morningData, WIDTH, HEIGHT);
+
+    const phaseMap = new Map();
+    phaseMap.set(1, [morning, nightGrid]);
+    phaseMap.set(-1, [night, morningGrid]);
+    return [ctx, WIDTH, HEIGHT, phaseMap];
+  };
+
+  const init = (ctx, WIDTH, HEIGHT) => {
     return Array.from(
       { length: WIDTH },
       () => new Particle(ctx, WIDTH, HEIGHT)
@@ -12,40 +53,6 @@ export const { handleAnimation, stopAnimation } = (() => {
     ctx.drawImage(img, 0, 0, WIDTH, HEIGHT);
   };
 
-  const getRgbsFromImageData = (imageArray, WIDTH, HEIGHT) => {
-    const colorMap = [];
-    for (let y = 0; y < HEIGHT; y++) {
-      const row = [];
-      for (let x = 0; x < WIDTH; x++) {
-        const r = imageArray.data[y * 4 * imageArray.width + x * 4];
-        const g = imageArray.data[y * 4 * imageArray.width + x * 4 + 1];
-        const b = imageArray.data[y * 4 * imageArray.width + x * 4 + 2];
-        const color = `rgb(${r},${g},${b})`;
-        row.push(color);
-      }
-      colorMap.push(row);
-    }
-    return colorMap;
-  };
-
-  const setup = () => {
-    const night = document.getElementById("night");
-    const morning = document.getElementById("morning");
-
-    drawImg(night);
-    const nightData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
-    const nightGrid = getRgbsFromImageData(nightData, WIDTH, HEIGHT);
-
-    drawImg(morning);
-    const morningData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
-    const morningGrid = getRgbsFromImageData(morningData, WIDTH, HEIGHT);
-
-    const phaseMap = new Map();
-    phaseMap.set(1, [morning, nightGrid]);
-    phaseMap.set(-1, [night, morningGrid]);
-    return phaseMap;
-  };
-
   const handleAnimation = () => {
     stopAnimation();
     setPhase(-phaseSign);
@@ -54,7 +61,7 @@ export const { handleAnimation, stopAnimation } = (() => {
   const setPhase = (newPhaseSign) => {
     phaseSign = newPhaseSign;
     phase = phaseMap.get(phaseSign)[1];
-    particles = init();
+    particles = init(ctx, WIDTH, HEIGHT);
     play();
   };
 
@@ -69,7 +76,7 @@ export const { handleAnimation, stopAnimation } = (() => {
     setTimeout(() => {
       onAnimate = true;
       animate();
-    }, 40);
+    }, 60);
   };
 
   const animate = () => {
@@ -83,18 +90,11 @@ export const { handleAnimation, stopAnimation } = (() => {
     particle.draw(phase);
   };
 
-  const canvas = document.querySelector(".mode__canvas");
-  const ctx = canvas.getContext("2d", { willReadFrequently: true });
-  canvas.width = 140;
-  canvas.height = 80;
-  const WIDTH = canvas.width;
-  const HEIGHT = canvas.height;
+  const [ctx, WIDTH, HEIGHT, phaseMap] = setup();
   let phaseSign = -1;
-  let onAnimate = false;
-  let particles;
-  const phaseMap = setup();
   let phase = phaseMap.get(phaseSign)[1];
-  particles = init();
+  let onAnimate = false;
+  let particles = init(ctx, WIDTH, HEIGHT);
 
   return { handleAnimation, stopAnimation };
 })();
--- src/themeButton/style.js
+++ src/themeButton/style.js
@@ -1,5 +1,15 @@
-export const { displayCanvas, foldCanvas, handleSign } = (() => {
+export const { displayCanvas, foldCanvas, handleThemeSign } = (() => {
   const setup = () => {
+    const light = () => {
+      const body = document.querySelector("body");
+      body.removeAttribute("data-theme");
+    };
+
+    const dark = () => {
+      const body = document.querySelector("body");
+      body.setAttribute("data-theme", "dark");
+    };
+
     const sunImage = "./public/img/sun.png";
     const moonImage = "./public/img/moon.png";
     const phaseMap = new Map();
@@ -8,18 +18,8 @@ export const { displayCanvas, foldCanvas, handleSign } = (() => {
     return phaseMap;
   };
 
-  const light = () => {
-    const body = document.querySelector("body");
-    body.removeAttribute("data-theme");
-  };
-
-  const dark = () => {
-    const body = document.querySelector("body");
-    body.setAttribute("data-theme", "dark");
-  };
-
-  const changeTheme = (sign) => {
-    phaseMap.get(sign)[1]();
+  const setTheme = (themeSign) => {
+    phaseMap.get(themeSign)[1]();
   };
 
   const displayCanvas = () => {
@@ -35,32 +35,32 @@ export const { displayCanvas, foldCanvas, handleSign } = (() => {
     }, 400);
   };
 
-  const changeButtonImage = (sign) => {
+  const setButtonImage = (themeSign) => {
     const img = document.querySelector(".mode__button__image");
     const button = document.querySelector(".mode__button");
-    const src = phaseMap.get(sign)[0];
     button.classList.add("clicked");
     setTimeout(() => {
+      const src = phaseMap.get(themeSign)[0];
       img.src = src;
       button.classList.remove("clicked");
     }, 400);
   };
 
-  const setSign = (newSign) => {
-    if (sign !== newSign) {
-      sign = newSign;
-      changeTheme(sign);
-      changeButtonImage(sign);
+  const setThemeSign = (newThemeSign) => {
+    if (themeSign !== newThemeSign) {
+      themeSign = newThemeSign;
+      setTheme(themeSign);
+      setButtonImage(themeSign);
     }
   };
 
-  const handleSign = () => {
-    setSign(-sign);
+  const handleThemeSign = () => {
+    setThemeSign(-themeSign);
   };
 
   let timeoutId;
-  let sign = 1;
+  let themeSign = 1;
   let phaseMap = setup();
 
-  return { displayCanvas, foldCanvas, handleSign };
+  return { displayCanvas, foldCanvas, handleThemeSign };
 })();
