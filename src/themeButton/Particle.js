108413e890180629e31acb4ec8af99ae8f3f3e20
hamelln <vusdus91@gmail.com>
2023-07-04 16:52:08 +0900

    refactor: 파티클 지역 변수 작법 변경

--- src/themeButton/Particle.js
+++ src/themeButton/Particle.js
@@ -1,41 +1,50 @@
 class Particle {
+  #fullAngle = Math.PI * 2;
+  #r = 5;
+  #speed = 1;
+  #ctx;
+  #WIDTH;
+  #HEIGHT;
+  #x;
+  #y;
+  #px;
+  #py;
+  #direction;
+
   constructor(ctx, WIDTH, HEIGHT) {
-    this.ctx = ctx;
-    this.WIDTH = WIDTH;
-    this.HEIGHT = HEIGHT;
-    this.fullAngle = Math.PI * 2;
-    this.x = Math.random() * this.WIDTH;
-    this.y = Math.random() * this.HEIGHT;
-    this.r = 5;
-    this.speed = 1;
-    this.direction = this.x > 63 ? 1 : -1;
-    this.px = Math.floor(this.x);
-    this.py = Math.floor(this.y);
+    this.#ctx = ctx;
+    this.#WIDTH = WIDTH;
+    this.#HEIGHT = HEIGHT;
+    this.#x = Math.random() * this.#WIDTH;
+    this.#y = Math.random() * this.#HEIGHT;
+    this.#px = Math.floor(this.#x);
+    this.#py = Math.floor(this.#y);
+    this.#direction = this.#x > 63 ? 1 : -1;
   }
 
   update(phaseSign) {
-    this.speed = this.speed <= 0.1 ? 0.1 : this.speed - 0.004;
-    this.x += this.direction * this.speed;
-    this.r = this.r <= 1 ? 1 : this.r - 0.04;
+    this.#speed = this.#speed <= 0.1 ? 0.1 : this.#speed - 0.004;
+    this.#x += this.#direction * this.#speed;
+    this.#r = this.#r <= 1 ? 1 : this.#r - 0.04;
 
-    if (this.x <= 0 || this.x > this.WIDTH) {
-      this.x = Math.random() * this.WIDTH;
-      this.y = Math.random() * this.HEIGHT;
-      this.direction *= -1;
-      if (this.speed < 0.2 && phaseSign > 0) {
-        this.y = this.HEIGHT / 2 + this.y / 2;
-        this.r = 2;
+    if (this.#x <= 0 || this.#x > this.#WIDTH) {
+      this.#x = Math.random() * this.#WIDTH;
+      this.#y = Math.random() * this.#HEIGHT;
+      this.#direction *= -1;
+      if (this.#speed < 0.2 && phaseSign > 0) {
+        this.#y = this.#HEIGHT / 2 + this.#y / 2;
+        this.#r = 2;
       }
     }
-    this.px = Math.floor(this.x);
-    this.py = Math.floor(this.y);
+    this.#px = Math.floor(this.#x);
+    this.#py = Math.floor(this.#y);
   }
 
   draw(phase) {
-    this.ctx.beginPath();
-    this.ctx.fillStyle = phase[this.py][this.px];
-    this.ctx.arc(this.x, this.y, this.r, 0, this.fullAngle);
-    this.ctx.fill();
+    this.#ctx.beginPath();
+    this.#ctx.fillStyle = phase[this.#py][this.#px];
+    this.#ctx.arc(this.#x, this.#y, this.#r, 0, this.#fullAngle);
+    this.#ctx.fill();
   }
 }
 
