d2740ee9c51999deafaeb7c1cb30f577621ac498
hamelln <vusdus91@gmail.com>
2023-07-06 11:39:31 +0900

    feat: 프로젝트 포커스에 따라 사용 기술 텍스트 변경

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import themeButtonSetup from "./themeButton/index.js";
 import setupHeaderScrollBehavior from "./setupHeaderScrollBehavior.js";
 import addKeyboardNavigationWithScroll from "./addKeyboardNavigationWithScroll.js";
+import setupOfProjectSkillTextChange from "./project/index.js";
 
 window.addEventListener("load", () => {
   themeButtonSetup();
   setupHeaderScrollBehavior();
   addKeyboardNavigationWithScroll();
+  setupOfProjectSkillTextChange();
   const op = document.querySelector(".opening");
   setTimeout(() => op.remove(), 6000);
 });
---
+++ src/project/index.js
@@ -0,0 +1,38 @@
+const setupOfProjectSkillTextChange = () => {
+  const projects = new Map();
+  projects.set("COFFEEN", {
+    uuid: "95000cc1-beb9-4c77-8812-13a8be2d5a16",
+    skills: "Next.js React TypeScript",
+    member: 1,
+    period: "6개월",
+  });
+  projects.set("반려in", {
+    uuid: "6bb4b7ce-a576-423c-96d1-95fc5f376f35",
+    skills: "HTML CSS JavaScript",
+    member: 6,
+    period: "2주",
+  });
+  projects.set("Modak", {
+    uuid: "2d36e029-2d89-48a0-a919-7ad06030fe9b",
+    skills: "Next.js React TypeScript Redux",
+    member: 5,
+    period: "1개월",
+  });
+
+  const skills = document.querySelector(".project__skills");
+  const projectItems = document.querySelectorAll(
+    ".project-content__selection__item"
+  );
+
+  projectItems.forEach((project) => {
+    project.addEventListener("focus", () => {
+      if (project.classList.contains("focus")) {
+        const projectTitle = project.textContent.trim();
+        const skillText = projects.get(projectTitle)["skills"];
+        skills.textContent = skillText;
+      }
+    });
+  });
+};
+
+export default setupOfProjectSkillTextChange;
