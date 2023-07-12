7983c9a0089fcfd255b37a5be75fade33c96413d
hamelln <vusdus91@gmail.com>
2023-07-12 15:23:29 +0900

    refactor: 데이터를 따로 json으로 분리

--- src/project/index.js
+++ src/project/index.js
@@ -1,30 +1,11 @@
 "use strict";
 
 import { addFocus, removeFocus } from "../utils/focus.js";
+import data from "../data/project.json" assert { type: "json" };
+import Project from "./Project.js";
 
 const setupChangeProjectSkill = () => {
-  const projects = new Map();
-  projects.set("COFFEEN", {
-    uuid: "95000cc1-beb9-4c77-8812-13a8be2d5a16",
-    skills: "Next.js React TypeScript",
-    member: 1,
-    period: "6개월",
-  });
-  projects.set("반려in", {
-    uuid: "6bb4b7ce-a576-423c-96d1-95fc5f376f35",
-    skills: "HTML CSS JavaScript",
-    member: 6,
-    period: "2주",
-  });
-  projects.set("Modak", {
-    uuid: "2d36e029-2d89-48a0-a919-7ad06030fe9b",
-    skills: "Next.js React TypeScript Redux",
-    member: 5,
-    period: "1개월",
-  });
-
   const isMobileDevice = /Mobi/i.test(navigator.userAgent);
-  const focusEvent = isMobileDevice ? "click" : "focus";
   const skills = document.querySelector(".project__skills");
   const projectItems = document.querySelectorAll(
     ".project-content__selection__item"
@@ -32,7 +13,7 @@ const setupChangeProjectSkill = () => {
 
   const changeProjectSkill = (project) => {
     const projectTitle = project.textContent.trim();
-    const skillText = projects.get(projectTitle)["skills"];
+    const skillText = data[projectTitle].spec.skill;
     skills.textContent = skillText;
   };
 
@@ -45,7 +26,11 @@ const setupChangeProjectSkill = () => {
       skills.textContent = "";
       removeFocus(project);
     });
-    project.addEventListener("click", () => {});
+    project.addEventListener("click", (e) => {
+      const projectTitle = e.target.textContent.trim();
+      const projectData = data[projectTitle];
+      Project(projectData);
+    });
   });
 };
 
