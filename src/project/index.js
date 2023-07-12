34aff009d234c70d16af0090240be0da1e535052
hamelln <vusdus91@gmail.com>
2023-07-13 00:59:27 +0900

    feat: mutation 옵저버 추가

--- src/project/index.js
+++ src/project/index.js
@@ -1,39 +1,59 @@
 "use strict";
 
-import { addFocus, removeFocus } from "../utils/focus.js";
 import data from "../data/project.json" assert { type: "json" };
+import addKeyboardNavigationWithScroll from "../ui/addKeyboardNavigationWithScroll.js";
+import { addFocus, removeFocus } from "../utils/focus.js";
+import { play } from "../utils/sound.js";
 import Project from "./Project.js";
+import Selection from "./Selection.js";
 
 const setupChangeProjectSkill = () => {
   const isMobileDevice = /Mobi/i.test(navigator.userAgent);
-  const focusEvent = isMobileDevice ? "click" : "focus";
+  const main = document.querySelector(".project-content");
+  const selectSound = document.getElementById("project-sound");
+  const startSound = document.getElementById("game-start");
   const skills = document.querySelector(".project__skills");
-  const projectItems = document.querySelectorAll(
-    ".project-content__selection__item"
-  );
 
-  const changeProjectSkill = (project) => {
-    const projectTitle = project.textContent.trim();
+  const displayProjectSkill = (projectTitle) => {
     const skillText = data[projectTitle].spec.skill;
     skills.textContent = skillText;
   };
 
-  projectItems.forEach((project) => {
-    project.addEventListener("focus", () => {
-      changeProjectSkill(project);
-      isMobileDevice && addFocus(project);
-    });
-    project.addEventListener("blur", () => {
-      skills.textContent = "";
-      removeFocus(project);
-    });
-    project.addEventListener("click", (e) => {
-      const projectTitle = e.target.textContent.trim();
-      const projectData = data[projectTitle];
-      Project(projectData);
-    });
-    project.addEventListener("click", () => {});
-  });
+  const mutationCallback = (mutationList) => {
+    const changedClassName = mutationList[1].addedNodes[0].className;
+    if (changedClassName === "project-content__selection") {
+      const projectList = document.querySelectorAll(
+        ".project-content__selection__item"
+      );
+
+      projectList.forEach((project) => {
+        const projectTitle = project.textContent.trim();
+
+        project.addEventListener("focus", () => {
+          displayProjectSkill(projectTitle);
+          isMobileDevice && addFocus(project);
+          play(selectSound);
+        });
+        project.addEventListener("blur", () => {
+          skills.textContent = "";
+          removeFocus(project);
+        });
+        project.addEventListener("click", () => {
+          const projectData = data[projectTitle];
+          play(startSound);
+          Project(projectData);
+        });
+      });
+
+      isMobileDevice || addKeyboardNavigationWithScroll();
+    }
+  };
+
+  const observer = new MutationObserver(mutationCallback);
+  const config = { childList: true, subtree: true };
+  observer.observe(main, config);
+
+  Selection();
 };
 
 export default setupChangeProjectSkill;
