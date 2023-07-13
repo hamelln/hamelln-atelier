b9c29f2b7889adf95be9dbd2e88d5997d89a16e1
hamelln <vusdus91@gmail.com>
2023-07-13 13:35:22 +0900

    fix: 화면 변경 시 이벤트 리스너 중복 등록이 안 되도록 전체 초기화

--- src/ui/addKeyboardNavigationWithScroll.js
+++ src/ui/addKeyboardNavigationWithScroll.js
@@ -2,64 +2,73 @@
 
 import { addFocus, removeFocus } from "../utils/focus.js";
 
-const addKeyboardNavigationWithScroll = () => {
+const handleKeyDown = (event) => {
   const focusableNodes = document.querySelectorAll(".focusable");
   const focusableArray = Array.from(focusableNodes);
+  navigateNodes(event, focusableArray);
+};
 
-  const navigateNodes = (event) => {
-    const currentIndex = focusableArray.findIndex(
-      (node) => node === document.activeElement
-    );
-    let nextIndex = -1;
+const navigateNodes = (event, focusableArray) => {
+  const currentIndex = focusableArray.findIndex(
+    (node) => node === document.activeElement
+  );
+  let nextIndex = -1;
 
-    event.preventDefault();
+  event.preventDefault();
 
-    switch (event.key) {
-      case "ArrowUp":
-        nextIndex = currentIndex - 1;
-        break;
-      case "ArrowDown":
-        nextIndex = currentIndex + 1;
-        break;
-      case "ArrowLeft":
-        nextIndex = currentIndex - 1;
-        break;
-      case "ArrowRight":
-        nextIndex = currentIndex + 1;
-        break;
-      case "Tab":
-        nextIndex = currentIndex + 1;
-        break;
-      default:
-        return;
-    }
+  switch (event.key) {
+    case "ArrowUp":
+      nextIndex = currentIndex - 1;
+      break;
+    case "ArrowDown":
+      nextIndex = currentIndex + 1;
+      break;
+    case "ArrowLeft":
+      nextIndex = currentIndex - 1;
+      break;
+    case "ArrowRight":
+      nextIndex = currentIndex + 1;
+      break;
+    case "Tab":
+      nextIndex = currentIndex + 1;
+      break;
+    default:
+      return;
+  }
 
-    if (nextIndex < 0) {
-      nextIndex = focusableArray.length - 1;
-    } else if (nextIndex >= focusableArray.length) {
-      nextIndex = 0;
-    }
-    removeFocus(focusableArray[currentIndex]);
-    const nextNode = focusableArray[nextIndex];
-    addFocus(nextNode);
-    const parentSection = nextNode.closest("section");
-    parentSection?.scrollIntoView({ block: "nearest" });
-  };
+  if (nextIndex < 0) {
+    nextIndex = focusableArray.length - 1;
+  } else if (nextIndex >= focusableArray.length) {
+    nextIndex = 0;
+  }
+  removeFocus(focusableArray[currentIndex]);
+  const nextNode = focusableArray[nextIndex];
+  addFocus(nextNode);
+  const parentSection = nextNode.closest("section");
+  parentSection?.scrollIntoView({ block: "nearest" });
+};
+
+const addKeyboardNavigationWithScroll = () => {
+  const focusableNodes = document.querySelectorAll(".focusable");
 
   focusableNodes.forEach((node) => {
-    node.addEventListener("mousemove", () => {
+    const handleFocus = () => {
       addFocus(node);
-    });
-    node.addEventListener("mouseleave", () => {
+    };
+    const handleBlur = () => {
       removeFocus(node);
-    });
-    node.addEventListener("focus", () => {
-      addFocus(node);
-    });
-    node.addEventListener("blur", () => {
-      removeFocus(node);
-    });
-    node.addEventListener("keydown", navigateNodes);
+    };
+    node.removeEventListener("mousemove", handleFocus);
+    node.removeEventListener("mouseleave", handleBlur);
+    node.removeEventListener("focus", handleFocus);
+    node.removeEventListener("blur", handleBlur);
+    node.removeEventListener("keydown", handleKeyDown);
+
+    node.addEventListener("mousemove", handleFocus);
+    node.addEventListener("mouseleave", handleBlur);
+    node.addEventListener("focus", handleFocus);
+    node.addEventListener("blur", handleBlur);
+    node.addEventListener("keydown", handleKeyDown);
   });
 };
 
