56a819824cc4296eaf0b8512d76f8999575b0631
hamelln <vusdus91@gmail.com>
2023-07-13 13:36:23 +0900

    feat: 프로젝트 선택 화면에 소리, 포커싱 등 기능 추가 완료

--- src/project/Selection.js
+++ src/project/Selection.js
@@ -1,4 +1,9 @@
 import data from "../data/project.json" assert { type: "json" };
+import addKeyboardNavigationWithScroll from "../ui/addKeyboardNavigationWithScroll.js";
+import addEventForClickAndEnter from "../utils/addEventForClickAndEnter.js";
+import { addFocus, removeFocus } from "../utils/focus.js";
+import { play } from "../utils/sound.js";
+import Project from "./Project.js";
 
 export default function Selection() {
   const makeBox = () => {
@@ -19,8 +24,9 @@ export default function Selection() {
     ulElem.classList.add("project-content__selection__list");
     const keys = Object.keys(data);
     keys.map((key) => {
-      const liElem = makeProjectItem(key);
-      ulElem.appendChild(liElem);
+      const projectItem = makeProjectItem(key);
+      addEventProjectItem(projectItem, key);
+      ulElem.appendChild(projectItem);
     });
     return ulElem;
   };
@@ -31,7 +37,6 @@ export default function Selection() {
     liElem.classList.add("focusable");
     liElem.tabIndex = 0;
     liElem.textContent = projectTitle;
-
     return liElem;
   };
 
@@ -55,6 +60,33 @@ export default function Selection() {
     return boxElem;
   };
 
+  const addEventProjectItem = (project, projectTitle) => {
+    const projectData = data[projectTitle];
+    const selectSound = document.getElementById("project-sound");
+    const startSound = document.getElementById("game-start");
+    const skills = document.querySelector(".project__skills");
+    const startProject = () => {
+      play(startSound);
+      Project(projectData);
+    };
+
+    const handleFocus = () => {
+      const skillText = projectData.spec.skill;
+      skills.textContent = skillText;
+      addFocus(project);
+      play(selectSound);
+    };
+
+    const handleBlur = () => {
+      skills.textContent = "";
+      removeFocus(project);
+    };
+
+    project.addEventListener("focus", handleFocus);
+    project.addEventListener("blur", handleBlur);
+    addEventForClickAndEnter(project)(startProject);
+  };
+
   const render = () => {
     const elem = document.querySelector(".project-content");
     elem.innerHTML = "";
@@ -62,6 +94,7 @@ export default function Selection() {
     const figure = makeFigure();
     elem.appendChild(box);
     elem.appendChild(figure);
+    addKeyboardNavigationWithScroll();
   };
   render();
 }
