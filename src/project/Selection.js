ee4ad8f97d901bae8998ec948d53d033cb21e367
hamelln <vusdus91@gmail.com>
2023-07-15 01:05:54 +0900

    refactor: 식별자 명 가독성 높이고, 함수 좀 더 나누고 분리함

--- src/project/Selection.js
+++ src/project/Selection.js
@@ -11,117 +11,127 @@ import {
   makeImg,
 } from "../utils/controllDOM.js";
 
+const createTitle = (className) => {
+  const titleElem = makeElementWithClasses("h2")(className);
+  return addAttribute(titleElem)({ text: "Project Select" });
+};
+
+const createProjectItem = (projectTitle) => {
+  const projectItem = makeElementWithClasses("li")(
+    "project-content__selection__item",
+    "focusable"
+  );
+  return addAttribute(projectItem)({ tabIndex: 0, text: projectTitle });
+};
+
+const insertProjectsIntoList = (projectList) => {
+  const projectArray = Object.keys(data);
+  projectArray.map((projectTitle) => {
+    const projectItem = createProjectItem(projectTitle);
+    projectList.appendChild(projectItem);
+  });
+  return projectList;
+};
+
+const addEventProjectItem = (project) => {
+  const projectTitle = project.textContent.trim();
+  const projectData = data[projectTitle];
+  const projectSkill = projectData.spec.skill.split(", ");
+  const selectSound = document.querySelector("#project-sound");
+  const startSound = document.querySelector("#game-start");
+  const describe = document.querySelector(".project__describe");
+  const ulElem = document.querySelector(".project-content__overview__skill");
+  const startProject = () => {
+    play(startSound);
+    Loading("Hamelln");
+    setTimeout(() => {
+      Project(projectData);
+    }, 1000);
+  };
+
+  const handleFocus = () => {
+    addAttribute(describe)({ text: projectData.describe });
+
+    projectSkill.map((skill) => {
+      const liElem = makeElementWithClasses("li")(
+        "project-content__overview__skill__item"
+      );
+      addAttribute(liElem)({ text: skill });
+      ulElem.appendChild(liElem);
+    });
+
+    play(selectSound);
+  };
+
+  const handleBlur = () => {
+    describe.textContent = "";
+    ulElem.innerHTML = "";
+  };
+
+  addFocusHandler(project)(handleFocus);
+  addBlurHandler(project)(handleBlur);
+  addClickAndEnterHandler(project)(startProject);
+};
+
+const findPrevFocusItem = (className, textContent) => {
+  const items = Array.from(document.querySelectorAll(className));
+  return items.find((item) => item.textContent.trim() === textContent);
+};
+
+const focusPreviousItem = (projectTitle) => {
+  if (!projectTitle) return;
+  const focusedItem = findPrevFocusItem(
+    ".project-content__selection__item",
+    projectTitle
+  );
+  addFocus(focusedItem);
+};
+
+const createSelections = () => {
+  const selectionBox = makeElementWithClasses("div")(
+    "project-content__selection"
+  );
+  const selectionTitle = createTitle("project-content__selection__title");
+  const selectionList = makeElementWithClasses("ul")(
+    "project-content__selection__list"
+  );
+  return { selectionBox, selectionTitle, selectionList };
+};
+
+const createFigureItems = () => {
+  const figure = makeElementWithClasses("figure")("project-content__overview");
+  const projectImg = makeImg("project-content__overview__image")(
+    "./public/img/cafe.png",
+    "project image"
+  );
+  const projectSkill = makeElementWithClasses("ul")(
+    "project-content__overview__skill"
+  );
+  return { figure, projectImg, projectSkill };
+};
+
+const render = (parent, projectTitle) => {
+  const { selectionBox, selectionTitle, selectionList } = createSelections();
+  const { figure, projectImg, projectSkill } = createFigureItems();
+
+  insertProjectsIntoList(selectionList);
+  selectionBox.appendChild(selectionTitle);
+  selectionBox.appendChild(selectionList);
+  figure.appendChild(projectImg);
+  figure.appendChild(projectSkill);
+
+  parent.innerHTML = "";
+  parent.appendChild(selectionBox);
+  parent.appendChild(figure);
+
+  selectionList.childNodes.forEach((project) => {
+    addEventProjectItem(project, projectTitle);
+  });
+  addKeyboardController();
+  focusPreviousItem(projectTitle);
+};
+
 export default function Selection(projectTitle = undefined) {
-  const makeTitle = (className) => {
-    const titleElem = makeElementWithClasses("h2")(className);
-    return addAttribute(titleElem)({ text: "Project Select" });
-  };
-
-  const makeProjectItem = (projectTitle) => {
-    const projectItem = makeElementWithClasses("li")(
-      "project-content__selection__item",
-      "focusable"
-    );
-    return addAttribute(projectItem)({ tabIndex: 0, text: projectTitle });
-  };
-
-  const insertProjectsIntoList = (projectList) => {
-    const projectArray = Object.keys(data);
-    projectArray.map((projectTitle) => {
-      const projectItem = makeProjectItem(projectTitle);
-      projectList.appendChild(projectItem);
-    });
-    return projectList;
-  };
-
-  const addEventProjectItem = (project) => {
-    const projectTitle = project.textContent.trim();
-    const projectData = data[projectTitle];
-    const projectSkill = projectData.spec.skill.split(", ");
-    const selectSound = document.querySelector("#project-sound");
-    const startSound = document.querySelector("#game-start");
-    const describe = document.querySelector(".project__describe");
-    const ulElem = document.querySelector(".project-content__overview__skill");
-    const startProject = () => {
-      play(startSound);
-      Loading("Hamelln");
-      setTimeout(() => {
-        Project(projectData);
-      }, 1000);
-    };
-
-    const handleFocus = () => {
-      addAttribute(describe)({ text: projectData.describe });
-
-      projectSkill.map((skill) => {
-        const liElem = makeElementWithClasses("li")(
-          "project-content__overview__skill__item"
-        );
-        addAttribute(liElem)({ text: skill });
-        ulElem.appendChild(liElem);
-      });
-
-      play(selectSound);
-    };
-
-    const handleBlur = () => {
-      describe.textContent = "";
-      ulElem.innerHTML = "";
-    };
-
-    addFocusHandler(project)(handleFocus);
-    addBlurHandler(project)(handleBlur);
-    addClickAndEnterHandler(project)(startProject);
-  };
-
-  const prevFocus = (focusedProjectTitle) => {
-    if (!focusedProjectTitle) return;
-
-    const projectList = document.querySelectorAll(
-      ".project-content__selection__item"
-    );
-
-    for (const project of projectList) {
-      const title = project.textContent.trim();
-      if (title === focusedProjectTitle) {
-        addFocus(project);
-        return;
-      }
-    }
-  };
-
-  const render = () => {
-    const parent = document.querySelector(".project-content");
-    const selectionBox = makeElementWithClasses("div")(
-      "project-content__selection"
-    );
-    const selectionTitle = makeTitle("project-content__selection__title");
-    const selectionList = makeElementWithClasses("ul")(
-      "project-content__selection__list"
-    );
-    const figure = makeElementWithClasses("figure")(
-      "project-content__overview"
-    );
-    const projectImg = makeImg("project-content__overview__image")(
-      "./public/img/cafe.png"
-    )("project image");
-    const projectSkill = makeElementWithClasses("ul")(
-      "project-content__overview__skill"
-    );
-    selectionBox.appendChild(selectionTitle);
-    selectionBox.appendChild(selectionList);
-    insertProjectsIntoList(selectionList);
-    figure.appendChild(projectImg);
-    figure.appendChild(projectSkill);
-    parent.innerHTML = "";
-    parent.appendChild(selectionBox);
-    parent.appendChild(figure);
-    selectionList.childNodes.forEach((project) => {
-      addEventProjectItem(project, projectTitle);
-    });
-    addKeyboardController();
-    prevFocus(projectTitle);
-  };
-
-  render();
+  const parent = document.querySelector(".project-content");
+  render(parent, projectTitle);
 }
