91dd2237cfcdd94d6897cd60022a0e0bf308ae0e
hamelln <vusdus91@gmail.com>
2023-07-16 13:32:36 +0900

    refactor: 일부 이미지 lazy 로딩, mute버튼 클릭 시의 setupSound는 최상위 index에서 전달토록 변경, 로직 주입은 idle로 변경

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import Selection from "./project/Selection.js";
 import setupMuteButton from "./ui/muteButton.js";
 import setupThemeButton from "./ui/themeButton/index.js";
 import navigateWithScroll from "./utils/navigateWithScroll.js";
+import { setupSound } from "./utils/sound.js";
 import { restoreFocus } from "./utils/storeFocus.js";
 
 window.addEventListener("load", () => {
   Selection();
-  setupMuteButton();
-  setupThemeButton();
-  navigateWithScroll();
+  requestIdleCallback(() => {
+    setupMuteButton(setupSound);
+    setupThemeButton();
+    navigateWithScroll();
+  });
 });
 
 window.addEventListener("focus", restoreFocus);
