bcdfb52004164e111828e782b00b4fa7c6089e68
hamelln <vusdus91@gmail.com>
2023-07-16 21:24:52 +0900

    feat: 모바일 구분 기능 추가, 모바일용 캐러셀 UI 추가

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@
 import Selection from "./project/Selection.js";
 import setupMuteButton from "./ui/muteButton.js";
 import setupThemeButton from "./ui/themeButton/index.js";
+import isMobile from "./utils/isMobile.js";
 import navigateWithScroll from "./utils/navigateWithScroll.js";
 import { setupSound } from "./utils/sound.js";
 import { restoreFocus } from "./utils/storeFocus.js";
 
 window.addEventListener("load", () => {
-  Selection();
+  if (!isMobile()) Selection();
+  else console.log("it's mobile");
   requestIdleCallback(() => {
     setupMuteButton(setupSound);
     setupThemeButton();
