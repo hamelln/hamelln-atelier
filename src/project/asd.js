0065bdbbfef3bbf692a37fa65539d4b94830b9e2
hamelln <vusdus91@gmail.com>
2023-07-18 02:03:12 +0900

    fix: 캐러셀 조정중

--- src/project/asd.js
+++ src/project/asd.js
@@ -1,19 +1,40 @@
-const allLabels = document.querySelectorAll(".project-content__carousel__card");
-const labelArray = Array.from(allLabels);
-const findActiveIndex = () => labelArray.findIndex(isActive);
+const findActiveIndex = () => LABEL_ARRAY.findIndex(isActive);
 const isActive = (label) => label.classList.contains("active");
-const isNext = (label) => label.classList.contains("next");
-const isPrev = (label) => label.classList.contains("prev");
 const findLabelByInputId = (id) => document.querySelector(`label[for=${id}]`);
 const openProject = (label) => {
   if (isActive(label)) console.log("welcome!");
 };
 const slide = () => {};
+const resetClass = () => {
+  getLabels().forEach((label) => {
+    label.classList.remove("prev");
+    label.classList.remove("active");
+    label.classList.remove("next");
+  });
+};
+const getLabels = () =>
+  document.querySelectorAll(".project-content__carousel__card");
 
 const allRadio = document.querySelectorAll("input");
+
+const LABEL_ARRAY = Array.from(getLabels());
+const ARR_LENGTH = LABEL_ARRAY.length;
+const findLabelByIndex = (index) => {
+  if (index < 0) return LABEL_ARRAY[ARR_LENGTH + index];
+  else if (index >= ARR_LENGTH) return LABEL_ARRAY[index % ARR_LENGTH];
+  return LABEL_ARRAY[index];
+};
+
 allRadio.forEach((radio) => {
   radio.addEventListener("change", () => {
-    const labelElement = document.querySelector(`label[for=${radio.id}]`);
+    const labelElement = findLabelByInputId(radio.id);
+    const activeIndex = LABEL_ARRAY.findIndex(
+      (label) => label === labelElement
+    );
+    resetClass();
     console.log(labelElement);
+    labelElement.classList.add("active");
+    findLabelByIndex(activeIndex - 1).classList.add("prev");
+    findLabelByIndex(activeIndex + 1).classList.add("next");
   });
 });
