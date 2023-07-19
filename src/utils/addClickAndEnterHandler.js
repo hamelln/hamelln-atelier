d758ae7ff171738a9caf764d30cd2c54eb196e47
hamelln <vusdus91@gmail.com>
2023-07-20 01:11:09 +0900

    fix: 불필요한 코드 삭제 및 변경

--- src/utils/addClickAndEnterHandler.js
+++ src/utils/addClickAndEnterHandler.js
@@ -1,14 +1,14 @@
 export default function addClickAndEnterHandler(element) {
   return (callback, ...args) => {
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
+      callback(...args);
+    });
     element.addEventListener("keydown", (e) => {
       if (e.key === "Enter") {
         e.preventDefault();
         callback(...args);
       }
-    });
-    element.addEventListener("click", (e) => {
-      e.preventDefault();
-      callback(...args);
     });
   };
 }
