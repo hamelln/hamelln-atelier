6e0149c0f1a2e93b4b2511719aafae8ac3337269
hamelln <vusdus91@gmail.com>
2023-07-21 13:36:00 +0900

    chore: 데이터 import 문 삭제 - 사파리 브라우저에서 assert 문 인식 못하는 것으로 추정 - 임시로 데이터 stub 생성, 테스트

--- src/project/MobileSelection.js
+++ src/project/MobileSelection.js
@@ -1,8 +1,125 @@
-import data from "../data/project.json" assert { type: "json" };
 import createElement from "../handlers/element-creater.js";
 import { play } from "../handlers/sound-handler.js";
 import Loading from "./Loading.js";
 import Project from "./Project.js";
+
+const data = {
+  Hamelln: {
+    uuid: "bea4bbcc-21f3-11ee-be56-0242ac120002",
+    title: "Hamelln",
+    siteUrl: "https://hamelln.vercel.app/",
+    codeUrl: "https://github.com/hamelln/hamelln",
+    docsUrl:
+      "https://separate-flea-6a7.notion.site/Hamelln-f06d994d44a64992a90d3d573ef03a50?pvs=4",
+    backgroundImage: "./public/img/about.png",
+    describe: "나를 소개하는 홈페이지",
+    skillOverview: ["HTML", "CSS", "JavaScript"],
+    spec: {
+      startDay: "2023.03",
+      endDay: "2023.07",
+      member: "1",
+      skill: ["HTML", "CSS", "JavaScript"],
+      role: "기획, 디자인, 프론트엔드",
+      characterImage: "./public/img/character.png",
+    },
+    feature: {
+      challenge:
+        "이 사이트는 크롬을 기준으로 만들었어요. 그래서 삼성 인터넷으로 켰을 때 스타일이 무너진 걸 봤죠.",
+      approach:
+        "스타일을 통일할 필요를 느꼈고, 이러한 크로스 브라우징 대책으로 normalize를 선택했어요.",
+      outcome: "그 결과 여러 브라우저에 동일한 경험을 제공할 수 있었습니다.",
+    },
+  },
+  COFFEEN: {
+    uuid: "95000cc1-beb9-4c77-8812-13a8be2d5a16",
+    title: "COFFEEN",
+    siteUrl: "https://coffeen.vercel.app/",
+    codeUrl: "https://github.com/hamelln/coffeen",
+    docsUrl:
+      "https://separate-flea-6a7.notion.site/COFFEEN-020f99bf6c1e47b194843fc5964fe799?pvs=4",
+    backgroundImage: "./public/img/COFFEEN.webp",
+    describe: "커피 간단 소개",
+    skillOverview: ["Next.js", "React", "TypeScript"],
+    spec: {
+      startDay: "2022.10",
+      endDay: "2023.04",
+      member: "1",
+      skill: [
+        "Next.js",
+        "React",
+        "TypeScript",
+        "Recoil",
+        "Styled-Components",
+        "Jest",
+      ],
+      role: "기획, 디자인, 프론트엔드",
+      characterImage: "./public/img/character.png",
+    },
+    feature: {
+      challenge:
+        "이 사이트는 크롬을 기준으로 만들었어요. 그래서 삼성 인터넷으로 켰을 때 스타일이 무너진 걸 봤죠.",
+      approach:
+        "스타일을 통일할 필요를 느꼈고, 이러한 크로스 브라우징 대책으로 normalize를 선택했어요.",
+      outcome: "그 결과 여러 브라우저에 동일한 경험을 제공할 수 있었습니다.",
+    },
+  },
+  Modak: {
+    uuid: "2d36e029-2d89-48a0-a919-7ad06030fe9b",
+    title: "Modak",
+    siteUrl: null,
+    codeUrl: null,
+    docsUrl:
+      "https://separate-flea-6a7.notion.site/fc3147e4613c4da7b3d6f8ae116331c9?pvs=4",
+    backgroundImage: "./public/img/modak.webp",
+    describe: "캠핑족에게 정보, 리뷰 제공",
+    skillOverview: ["Next.js", "React", "TypeScript"],
+    spec: {
+      startDay: "2022.01",
+      endDay: "2022.02",
+      member: "5",
+      skill: [
+        "Next.js",
+        "React",
+        "TypeScript",
+        "Redux",
+        "Styled-Components",
+        "Jest",
+      ],
+      role: "마이페이지",
+      characterImage: "./public/img/character.png",
+    },
+    feature: {
+      challenge: "",
+      approach: "",
+      outcome: "",
+    },
+  },
+  반려in: {
+    uuid: "6bb4b7ce-a576-423c-96d1-95fc5f376f35",
+    title: "반려in",
+    siteUrl: null,
+    codeUrl: null,
+    docsUrl:
+      "https://separate-flea-6a7.notion.site/in-86c4c117c2fe4135b000a2e1ac365762?pvs=4",
+    backgroundImage: "./public/img/banryeoin.webp",
+    bgmUrl: "./public/sound/bgm.mp3",
+    describe: "유기 동물 입양 커뮤니티",
+    skillOverview: ["HTML", "CSS", "JavaScript"],
+    spec: {
+      startDay: "2021.12.14",
+      endDay: "2021.12.25",
+      member: "6",
+      skill: ["HTML", "CSS", "JavaScript"],
+      role: "마이페이지",
+      characterImage: "./public/img/character.png",
+    },
+    feature: {
+      challenge: "",
+      approach: "",
+      outcome: "",
+    },
+  },
+};
 
 const SELECT_SOUND = document.querySelector("#project-sound");
 const START_SOUND = document.querySelector("#game-start");
