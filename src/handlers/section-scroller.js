344f535935abe21161006e76d87d77015cc151c0
hamelln <vusdus91@gmail.com>
2023-07-24 17:08:24 +0900

    fix: 스크롤 네비게이션, 포커싱 기능 수정 - 키보드 엔터로 누르면 해당 섹션의 첫 번째 아이템으로 포커싱 - 마우스로 누르면 스크롤 이동만 - 프로젝트 화면에서도 마우스로 hover하면 키보드 모드의 focus 해제

--- src/handlers/section-scroller.js
+++ src/handlers/section-scroller.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { addFocus } from "./focus-handler.js";
+
 const calcCenterPositionOfSection = (section) => {
   const windowHeight = window.innerHeight;
   const headerHeight = document.querySelector("header").offsetHeight;
@@ -16,20 +18,27 @@ const contact = document.getElementById("contact");
 const ABOUT_POSITION = calcCenterPositionOfSection(about);
 const PROJECT_POSITION = calcCenterPositionOfSection(project);
 const CONTACT_POSITION = calcCenterPositionOfSection(contact);
+const focusOnFirstItem = (section) => {
+  addFocus(section.querySelector(".focusable"));
+};
 
-const scrollToSection = (sectionId) => {
+const scrollToSection = (sectionId, isEnter) => {
   switch (sectionId) {
     case "about":
       scrollTo(0, ABOUT_POSITION);
+      isEnter && focusOnFirstItem(about);
       break;
     case "project":
       scrollTo(0, PROJECT_POSITION);
+      isEnter && focusOnFirstItem(project);
       break;
     case "contact":
       scrollTo(0, CONTACT_POSITION);
+      isEnter && focusOnFirstItem(contact);
       break;
     case "home":
       scrollTo(0, 0);
+      isEnter && focusOnFirstItem(document);
       break;
     default:
       break;
