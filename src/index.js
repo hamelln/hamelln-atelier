39997159cd83c264260b24e37d70c3c67d788801
hamelln <vusdus91@gmail.com>
2023-11-21 23:11:56 +0900

    refactor: index.js에서 이벤트 코드를 직접 짜는 대신 각 함수 가져오도록 분류

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ import { restoreFocus } from "./handlers/focus-saver.js";
 import setupSound from "./controllers/sound-controller.js";
 import addKeyboardController from "./controllers/keyboard-controller.js";
 import addClickAndEnterHandler from "./handlers/click-enter-handler.js";
+import scrollytellingTitleAndCanvas from "./ui/scrollytelling-title-and-canvas.js";
+import moveTextOnMouse from "./ui/move-text-onmouse.js";
+import scrollytellingLoadingBar from "./ui/scrollytelling-loading-bar.js";
+import handleTabsFocusAndHover from "./header/style-tabs.js";
+import scrollytellingAboutGuide from "./ui/scrollytelling-about-guide.js";
+import scrollytellingBackgroundImage from "./ui/scrollytelling-background-image.js";
+import scrollytellingAboutNavAndProject from "./ui/scrollytelling-about-nav-and-project.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   if (isMobile()) MobileHeader();
@@ -17,35 +24,31 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 window.addEventListener("load", () => {
-  window.addEventListener("scroll", () => {
-    const scroll = window.scrollY;
-    const windowHeight = window.innerHeight;
-    const documentHeight = document.body.clientHeight;
-    const width = Math.min(
-      (scroll / (documentHeight - windowHeight)) * 100,
-      100
-    );
-    const loadingBar = document.querySelector(".loading-bar");
-    loadingBar.style.width = width + "%";
-  });
-
-  const title = document.querySelector(".about__title");
-
-  window.addEventListener("scroll", () => {
-    const aboutHeight = document.getElementById("about").clientHeight;
-    const opacity = 1 - (scrollY / aboutHeight) * 4.5;
-    title.style.opacity = opacity;
-  });
-
   const settingButton = document.querySelector(".mode__setting");
   const themeBtn = document.querySelector(".mode__theme");
   const muteBtn = document.querySelector(".mode__mute");
+  const titleElement = document.querySelector(".about__title");
+  const HEIGHT_OF_ABOUT = document.getElementById("about").offsetHeight;
+  const START_Y_OF_TITLE_AND_CANVAS =
+    document.querySelector(".header").offsetHeight * 2;
+  const START_Y_OF_GUIDE =
+    START_Y_OF_TITLE_AND_CANVAS + HEIGHT_OF_ABOUT * (1 / 8);
+  const START_Y_OF_BACKGROUND = 168 + START_Y_OF_GUIDE;
+  const START_Y_OF_NAV = HEIGHT_OF_ABOUT / 2.4;
+
   addClickAndEnterHandler(settingButton)(() => {
     themeBtn.classList.toggle("focusable");
     muteBtn.classList.toggle("focusable");
     addKeyboardController();
   });
 
+  scrollytellingLoadingBar();
+  moveTextOnMouse(titleElement);
+  scrollytellingTitleAndCanvas(START_Y_OF_TITLE_AND_CANVAS, HEIGHT_OF_ABOUT);
+  scrollytellingAboutGuide(START_Y_OF_GUIDE);
+  scrollytellingBackgroundImage(START_Y_OF_BACKGROUND);
+  scrollytellingAboutNavAndProject(START_Y_OF_NAV);
+  handleTabsFocusAndHover();
   setupMuteButton(setupSound);
   setupThemeButton();
   navigateWithScroll();
