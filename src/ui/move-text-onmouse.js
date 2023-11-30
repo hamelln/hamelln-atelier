c89a4cc3e0a2edb7b8de9d9ebb3fdaf6b9ffe7d8
hamelln <vusdus91@gmail.com>
2023-11-30 16:13:39 +0900

    fix: mousemove와 mouseleave의 동작을 분리

--- src/ui/move-text-onmouse.js
+++ src/ui/move-text-onmouse.js
@@ -1,18 +1,20 @@
 const moveTextOnMouse = (textElement) => {
   function translateTitle(e) {
-    const { offsetX: x, offsetY: y } = e,
-      { offsetWidth: width, offsetHeight: height } = e.target,
-      move = 12,
-      xMove = (x / width) * (move * 8) - move * 4,
-      yMove = (y / height) * (move * 2) - move;
+    const { offsetX, offsetY, target } = e;
+    const { offsetWidth, offsetHeight } = target;
+    const move = 12;
+    const xMove = (offsetX / offsetWidth) * (move * 8) - move * 4;
+    const yMove = (offsetY / offsetHeight) * (move * 2) - move;
 
     e.target.style.transform = `translate(${xMove}px, ${yMove}px)`;
+  }
 
-    if (e.type === "mouseleave") e.target.style.transform = "";
+  function reset(e) {
+    e.target.style.transform = "";
   }
 
   textElement.addEventListener("mousemove", translateTitle);
-  textElement.addEventListener("mouseleave", translateTitle);
+  textElement.addEventListener("mouseleave", reset);
 };
 
 export default moveTextOnMouse;
