1b5e20af6ceee3e417ab372d46fe167559b62e17
hamelln <vusdus91@gmail.com>
2023-12-02 00:28:02 +0900

    fix: 헤더 포지션이 absolute로 변경됨에 따라 위치 계산 로직에서 헤더 제거 - about, home은 같이 두되 추후 기능 추가 예정(현재 미정)

--- src/handlers/section-scroller.js
+++ src/handlers/section-scroller.js
@@ -4,19 +4,15 @@ import { addFocus } from "./focus-handler.js";
 
 const calcCenterPositionOfSection = (section) => {
   const windowHeight = window.innerHeight;
-  const headerHeight = document.querySelector("header").offsetHeight;
   const sectionTop = section.offsetTop;
   const sectionHeight = section.offsetHeight;
-  const position =
-    sectionTop - (windowHeight - sectionHeight) / 2 + headerHeight / 2;
+  const position = sectionTop - (windowHeight - sectionHeight) / 2;
   return position;
 };
 
 const about = document.getElementById("about");
 const project = document.getElementById("project");
-const contact = document.getElementById("contact");
 const PROJECT_POSITION = calcCenterPositionOfSection(project);
-const CONTACT_POSITION = calcCenterPositionOfSection(contact);
 const focusOnFirstItem = (section) => {
   addFocus(section.querySelector(".focusable"));
 };
@@ -24,20 +20,13 @@ const focusOnFirstItem = (section) => {
 const scrollToSection = (sectionId, isEnter) => {
   switch (sectionId) {
     case "about":
+    case "home":
       scrollTo(0, 0);
       isEnter && focusOnFirstItem(about);
       break;
     case "project":
       scrollTo(0, PROJECT_POSITION);
       isEnter && focusOnFirstItem(project);
-      break;
-    case "contact":
-      scrollTo(0, CONTACT_POSITION);
-      isEnter && focusOnFirstItem(contact);
-      break;
-    case "home":
-      scrollTo(0, 0);
-      isEnter && focusOnFirstItem(about);
       break;
     default:
       break;
