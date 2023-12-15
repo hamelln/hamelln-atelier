07f3f089ce51abce974b6c2e4a45814cf6e389b8
hamelln <vusdus91@gmail.com>
2023-12-15 22:02:45 +0900

    refactor: 매번 함수 호출마다 이벤트 핸들러 함수를 생성하던 방식 -> 미리 만들어둔 것 재사용

--- src/controllers/keyboard-controller.js
+++ src/controllers/keyboard-controller.js
@@ -2,45 +2,43 @@
 
 import { addFocus, removeFocus } from "../handlers/focus-handler.js";
 import moveToNextFocus from "../handlers/keyboard-handler.js";
-import isMobile from "../handlers/mobile-recognizer.js";
 import scrollToSection from "../handlers/section-scroller.js";
 
-const addKeyboardController = () => {
-  if (isMobile()) return;
+//? 마우스 클릭 땐 포커스 이벤트 방지
+function preventFocusOnClick(e) {
+  e.preventDefault();
+}
 
+function handleBlur() {
+  removeFocus(this);
+}
+
+function handleHover() {
+  removeFocus(document.activeElement);
+}
+
+function handleFocus(e) {
+  const sectionId = e.target.closest("section")?.id;
+  sectionId && scrollToSection(sectionId);
+  addFocus(this);
+}
+
+const updateKeyboardController = () => {
   const focusableElements = document.querySelectorAll(".focusable");
 
   focusableElements.forEach((element) => {
-    //? 마우스 클릭 땐 포커스 이벤트 방지
-    const preventFocusOnClick = (e) => {
-      e.preventDefault();
-    };
-    const handleFocus = (e) => {
-      const sectionId = e.target.closest("section")
-        ? e.target.closest("section").id
-        : e.target.closest("footer");
-      sectionId && scrollToSection(sectionId);
-      addFocus(element);
-    };
-    const handleBlur = () => {
-      removeFocus(element);
-    };
-    const handleHover = () => {
-      removeFocus(document.activeElement);
-    };
-
-    element.removeEventListener("mousedown", preventFocusOnClick);
     element.removeEventListener("focus", handleFocus);
     element.removeEventListener("blur", handleBlur);
-    element.removeEventListener("keydown", moveToNextFocus);
     element.removeEventListener("mouseover", handleHover);
+    element.removeEventListener("mousedown", preventFocusOnClick);
+    element.removeEventListener("keydown", moveToNextFocus);
 
-    element.addEventListener("mousedown", preventFocusOnClick);
-    element.addEventListener("mouseover", handleHover);
     element.addEventListener("focus", handleFocus);
     element.addEventListener("blur", handleBlur);
+    element.addEventListener("mouseover", handleHover);
+    element.addEventListener("mousedown", preventFocusOnClick);
     element.addEventListener("keydown", moveToNextFocus);
   });
 };
 
-export default addKeyboardController;
+export default updateKeyboardController;
