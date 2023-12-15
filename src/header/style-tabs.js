e70255d085b168d0dc2914c921bd07e6bb0fedda
hamelln <vusdus91@gmail.com>
2023-12-15 22:03:21 +0900

    refactor: 스타일 조정 함수를 activeStyle로 명시하고 재사용. blur에 대한 처리 추가

--- src/header/style-tabs.js
+++ src/header/style-tabs.js
@@ -3,46 +3,28 @@ const handleTabsFocusAndHover = () => {
   let background = nav.querySelector(".header-nav__background");
   let items = nav.querySelectorAll(".header-nav-list__item");
 
+  function activeStyle() {
+    let rect = this.getBoundingClientRect();
+    let navRect = nav.getBoundingClientRect();
+    let coords = {
+      height: this.clientHeight,
+      width: this.clientWidth,
+      top: rect.top - navRect.top,
+      left: rect.left - navRect.left,
+    };
+    background.style.setProperty("width", `${coords.width}px`);
+    background.style.setProperty("height", `${coords.height}px`);
+    background.style.setProperty(
+      "transform",
+      `translate(${coords.left}px, ${coords.top}px)`
+    );
+    background.style.setProperty("opacity", "1");
+  }
+
   items.forEach((item) => {
-    item.addEventListener("focus", function () {
-      let rect = this.getBoundingClientRect();
-      let navRect = nav.getBoundingClientRect();
-      let coords = {
-        height: this.clientHeight,
-        width: this.clientWidth,
-        top: rect.top - navRect.top,
-        left: rect.left - navRect.left,
-      };
-
-      background.style.setProperty("width", `${coords.width}px`);
-      background.style.setProperty("height", `${coords.height}px`);
-      background.style.setProperty(
-        "transform",
-        `translate(${coords.left}px, ${coords.top}px)`
-      );
-      background.style.setProperty("opacity", "1");
-    });
-    item.addEventListener("mouseenter", function () {
-      let rect = this.getBoundingClientRect();
-      let navRect = nav.getBoundingClientRect();
-
-      let coords = {
-        height: this.clientHeight,
-        width: this.clientWidth,
-        top: rect.top - navRect.top,
-        left: rect.left - navRect.left,
-      };
-
-      background.style.setProperty("width", `${coords.width}px`);
-      background.style.setProperty("height", `${coords.height}px`);
-      background.style.setProperty(
-        "transform",
-        `translate(${coords.left}px, ${coords.top}px)`
-      );
-      background.style.setProperty("opacity", "1");
-    });
-
-    item.addEventListener("blur", function () {
+    item.addEventListener("focus", activeStyle);
+    item.addEventListener("mouseenter", activeStyle);
+    item.addEventListener("blur", () => {
       background.style.setProperty("opacity", "0");
     });
   });
